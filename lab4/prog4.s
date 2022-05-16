a61631606ebe49232181c3111cbdcfd7cecf88e1
idiiatullin.af <idiiatullin.af@unix.dozen>
2022-05-16 10:39:58 +0300

    qwerty

--- lab4/prog4.s
+++ lab4/prog4.s
@@ -2,7 +2,7 @@
 //A*(B-C)+A*(D-E)+F*(G+H)
 	.data
 mes1:
-	.string	"\nMatrix:\n"
+	.string	"Matrix:\n"
 mes2:
 	.string	"%lf"
 mes21:
@@ -10,9 +10,9 @@ mes21:
 mes:
 	.string "%d"
 mes0:
-	.string "n = %d\n"
+	.string "Size of matrixes = %d\n"
 mes3:
-	.string	"%.5g "
+	.string	"%.5lf "
 mes4:
 	.string	"\n"
 modeopen:
@@ -26,25 +26,101 @@ arg_error_msg:
 close_error_msg:
 	.string "\nError with closing file\n"
 size_error_msg:
-	.string "\nSize of matrixes can not be less than 1\n"
+	.string "\nSize of matrixes can not be = %d\n"
 data_error_msg:
 	.string "\nData in file are in incorrect form\n"
 empty_file_msg:
 	.string "\nFile is empty\n"
+eof_msg:
+	.string "\nEOF reached\n"
+msg1:
+	.string "\n//B-C\n"
+msg2:
+	.string "\n//A*(B-C)\n"
+msg3:
+	.string "\n//D-E\n"
+msg4:
+	.string "\n//A*(D-E)\n"
+msg5:
+	.string "\n//A*(B-C)+A*(D-E)\n"
+msg6:
+	.string "\n//G+H\n"
+msg7:
+	.string "\n//F*(G+H)\n"
+msg8:
+	.string "\n//A*(B-C)+A*(D-E)+F*(G+H)\n"
+A:
+	.skip 3200
+B:
+	.skip 3200
+C:
+	.skip 3200
+D:
+	.skip 3200
+E:
+	.skip 3200
+F:
+	.skip 3200
+G:
+	.skip 3200
+H:
+	.skip 3200
+buf_matrix1:
+	.skip 3200
+buf_matrix2:
+	.skip 3200
+buf_matrix3:
+	.skip 3200	
+				
 	.text
 	.align	2
 
 	.global mul_matrix
 	.type	mul_matrix, %function
 mul_matrix:
-	ret
+	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2, x0 - address of result Matrix
+	mov x10, #-1 //line number
+0:	
+	add x10, x10, #1 //i
+	mov x11, #0 //column number //j
+	cmp x10, x3
+	bge 2f
+	mul x12, x10, x3
+	lsl x12, x12, #3
+1:
+	cmp x11, x3
+	bge 0b
+	
+	//fmov d0, #0.0
+	movi d0, #0
+	mov x13, #0 //k
+3:
+	cmp x13, x3
+	bge 4f
+	add x14, x12, x13, lsl #3 //a[i][k]
+	ldr d1, [x1, x14]
+	mul x15, x13, x3
+	lsl x15, x15, #3
+	add x15, x15, x11, lsl #3 //b[k][j]
+	ldr d2, [x2, x15]
+	fmul d3, d1, d2
+	fadd d0, d0, d3
+	add x13, x13, #1
+	b 3b
+4:
+	add x12, x12, x11, lsl #3
+	str d0, [x0, x12]
+	add x11, x11, #1
+	b 1b
+2:
+	ret	
 	.size	mul_matrix, .-mul_matrix
 
 
 	.global sum_matrix
 	.type	sum_matrix, %function
 sum_matrix:	
-	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2 
+	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2, x0 - address of result Matrix 
 	mov x10, #-1 //line number
 0:	
 	add x10, x10, #1
@@ -71,7 +147,7 @@ sum_matrix:
 	.global sub_matrix
 	.type	sub_matrix, %function
 sub_matrix:
-	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2 
+	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2, x0 - address of result Matrix 
 	mov x10, #-1 //line number
 0:	
 	add x10, x10, #1
@@ -93,6 +169,109 @@ sub_matrix:
 2:	
 	ret	
 	.size	sub_matrix, .-sub_matrix		
+
+
+	.global make_matrix
+	.type	make_matrix, %function
+	.equ	x, 16
+	.equ	y, 24
+	.equ	z, 32
+	.equ	e, 40
+	.equ	a, 48
+	.equ	b, 56
+make_matrix:
+	//x0 - address of result Matrix, x1 - size, x2 - fd
+	stp	x29, x30, [sp, #-64]!
+	mov	x29, sp
+	stp x20, x21, [x29, x]
+	str x22, [x29, z]
+	stp x25, x26, [x29, a]
+	mov x20, x0
+	mov x21, x1
+	mov x22, x2
+	//get elements of matrixes
+	mov x25, #0 
+	mul x26, x21, x21 //count of elements in matrix
+0:	
+	cmp x25, x26
+	bge 5f
+	mov x0, x22
+	adr x1, mes2
+	add x2, x29, e
+	bl fscanf 
+	mov x1, #0xffffffff
+	cmp x0, x1
+	beq 2f
+	cmp x0, #0
+	beq 3f	
+	
+	ldr d0, [x29, e] //element
+	str d0, [x20, x25, lsl #3]
+	add x25, x25, #1
+	b 0b
+	
+2:	//empty file
+	mov x0, #-1
+	b 4f
+3:	//data_error	
+	mov x0, #0
+	b 4f
+5:
+	mov x0, x20		
+4:	
+	ldp x20, x21, [x29, x]
+	ldr x22, [x29, z]
+	ldp x25, x26, [x29, a]
+	ldp	x29, x30, [sp], #64
+	ret
+	.size	make_matrix, .-make_matrix
+
+
+	.global print_matrix
+	.type	print_matrix, %function
+	.equ	x, 16
+	.equ	y, 24
+	.equ	a, 32
+	.equ	b, 40
+print_matrix:
+	//x0 - address of result Matrix, x1 - size
+	stp	x29, x30, [sp, #-48]!
+	mov	x29, sp
+	stp	x20, x21, [x29, x]
+	stp x22, x23, [x29, a]
+	mov x20, x0
+	mov x21, x1
+	adr x0, mes1
+	bl printf 
+	
+	mov x22, #-1 //line number
+0:	
+	add x22, x22, #1
+	cmp x22, x21
+	bge 4f
+	mov x23, #0 //column number
+1:
+	cmp x23, x21
+	bge 2f
+	mul x12, x22, x21
+	lsl x12, x12, #3
+	add x12, x12, x23, lsl #3
+	ldr d0, [x20, x12]
+	adr x0, mes3
+	bl printf //element
+	add x23, x23, #1
+	b 1b
+2:
+	adr x0, mes4
+	bl printf
+	b 0b
+4:
+	mov x0, #0
+	ldp	x20, x21, [x29, x]
+	ldp x22, x23, [x29, a]
+	ldp	x29, x30, [sp], #48
+	ret
+	.size	print_matrix, .-print_matrix
 
 
 	.global	main
@@ -135,35 +314,183 @@ main:
 	cmp x0, #0
 	beq data_error
 	
-	ldrsw x3, [x29, x]
-	//
+	ldr x3, [x29, x]
+	mov x1, x3
+	cmp x3, #0
+	ble error_size
+	cmp x3, #20
+	bgt error_size
+
+	//print size
 	adr x0, mes0
 	mov x1, x3
 	bl printf
-	//
 
-	ldrsw x3, [x29, x]
-	//mov x0, #0
-	cmp x3, #0
-	ble error_size
+	//make matrix
+	//x0 - address of result Matrix, x1 - size, x2 - fd
+	adr x0, A
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
 
-	//get elements of matrixes
-	mov x0, x21
-	adr x1, mes2
-	add x2, x29, y
-	bl fscanf 
-	mov x1, #0xffffffff
-	cmp x0, x1
-	beq empty_file
+	adr x0, B
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
+	
+	adr x0, C
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error		
+
+	adr x0, D
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
+
+	adr x0, E
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
+
+	adr x0, F
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
 	cmp x0, #0
 	beq data_error	
 
-	ldr d0, [x29, y]
-	//
-	adr x0, mes21
-	bl printf
-	//
+	adr x0, G
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
 
+	adr x0, H
+	ldr x1, [x29, x]
+	mov x2, x21
+	bl make_matrix
+	cmp x0, #-1
+	beq eof 
+	cmp x0, #0
+	beq data_error
+
+	
+	//B-C
+	adr x0, msg1
+	bl printf
+	//x3 - size, x1 - address of Matrix1, x2 - address of Matrix2, x0 - address of result Matrix
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix2
+	adr x1, B
+	adr x2, C
+	bl sub_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix
+		
+	//A*(B-C)
+	adr x0, msg2
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix1  //A*(B-C)
+	adr x1, A
+	adr x2, buf_matrix2
+	bl mul_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//D-E
+	adr x0, msg3
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix2
+	adr x1, D
+	adr x2, E
+	bl sub_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//A*(D-E)
+	adr x0, msg4
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix3 //A*(D-E)
+	adr x1, A
+	adr x2, buf_matrix2
+	bl mul_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//A*(B-C)+A*(D-E)
+	adr x0, msg5
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix1 //A*(B-C)+A*(D-E)
+	adr x1, buf_matrix1
+	adr x2, buf_matrix3
+	bl sum_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//G+H
+	adr x0, msg6
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix2
+	adr x1, G
+	adr x2, H
+	bl sum_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//F*(G+H)
+	adr x0, msg7
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix3 //F*(G+H)
+	adr x1, F
+	adr x2, buf_matrix2
+	bl mul_matrix
+	ldrsw x1, [x29, x]
+	bl print_matrix	
+
+	//A*(B-C)+A*(D-E)+F*(G+H)
+	adr x0, msg8
+	bl printf
+	ldr x3, [x29, x]	
+	adr x0, buf_matrix1 //A*(B-C)+A*(D-E)+F*(G+H)
+	adr x1, buf_matrix1
+	adr x2, buf_matrix3
+	bl sum_matrix
+	//in x0 - address of matrix, x1 - size
+	ldrsw x1, [x29, x]
+	bl print_matrix
+	
 	//closing file
 	mov x0, x21
 	bl fclose
@@ -173,7 +500,11 @@ main:
 quit:
 	ldp	x29, x30, [sp], #32
 	ret
-
+eof:
+	adr x0, eof_msg
+	bl printf
+	mov w0, #1
+	b quit
 arg_error:
 	adr x0, arg_error_msg
 	mov x1, x24
